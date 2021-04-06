e641750df4a8272e00b7f19894943be92f739b31
Junior Vian <delcir.vian@neomind.com.br>
2021-04-05 22:55:24 -0300

    Criado componente de item da lista da página de conteúdo

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
+import HistoryCard from '../../components/HistoryCard';
 
-import {Container} from  './styles'
+import {Container, Content} from  './styles'
 
 const options = [
     {value: 'Juninho', label: 'Juninho'}
@@ -14,6 +15,30 @@ const List: React.FC = () => {
             <ContentHeader title="Saídas" lineColor="#E44C4E">
                 <SelectInput options={options}/>
             </ContentHeader>
+
+            <Content>
+                <HistoryCard 
+                    cardColor="#313862"
+                    tagColor="#E44C4e"
+                    title="Conta de Luz"
+                    subtitle="22/07/2020"
+                    amount="R$ 150,00"
+                />
+                <HistoryCard 
+                    cardColor="#313862"
+                    tagColor="#E44C4e"
+                    title="Conta de Luz"
+                    subtitle="22/07/2020"
+                    amount="R$ 175,00"
+                />
+                <HistoryCard 
+                    cardColor="#313862"
+                    tagColor="#E44C4e"
+                    title="Conta de Luz"
+                    subtitle="22/07/2020"
+                    amount="R$ 149,70"
+                />
+            </Content>
         </Container>
     );
 }
